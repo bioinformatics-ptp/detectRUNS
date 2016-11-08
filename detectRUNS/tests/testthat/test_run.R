6e4799c46c8dcf33fce5e68ff17e0e13e5c03ecb
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-11-08 17:35:26 +0100

    Add zzz.R - fixed test

--- detectRUNS/tests/testthat/test_run.R
+++ detectRUNS/tests/testthat/test_run.R
@@ -13,6 +13,14 @@ test_that("n_individuals is equal to 3", {
   # testing function
   expect_equal(n_individuals, 3)
 
+  # reading rohet, reference and test
+  reference_path <- system.file("extdata", "detected.ROHet.csv", package = "detectRUNS")
+  reference_rohet <- read.csv2(reference_path, header = T)
+  test_rohet <- read.csv2("detected.ROHet.csv", header = T)
+
+  # compare rohet table
+  expect_identical(test_rohet, reference_rohet)
+
   # cleanup
   file.remove("detected.ROHet.csv")
   file.remove("genotype.raw")
