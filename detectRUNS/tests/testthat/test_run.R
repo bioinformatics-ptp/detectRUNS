b5134d965534e1e1e9b65ea1583b3b0342b5636a
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-11-10 17:27:49 +0100

    Loading genotypes using bigmemory
    Former-commit-id: 5a05e24478d56ac6f19be7c8aacd73a12d556b67

--- detectRUNS/tests/testthat/test_run.R
+++ detectRUNS/tests/testthat/test_run.R
@@ -1,7 +1,7 @@
 library(detectRUNS)
 context("Testing RUNS")
 
-test_that("n_individuals is equal to 3", {
+test_that("detected ROHet are identical", {
   # loading dataset
   data("chillingam")
 
@@ -16,7 +16,19 @@ test_that("n_individuals is equal to 3", {
   # reading rohet, reference and test
   reference_path <- system.file("extdata", "detected.ROHet.csv", package = "detectRUNS")
   reference_rohet <- read.csv2(reference_path, header = T)
+
+  #  sort dataframe
+  # reference_rohet <- reference_rohet[order(reference_rohet$breed, reference_rohet$id), ]
+
+  # change row_names
+  # row.names(reference_rohet) <- 1:nrow(reference_rohet)
+
+  # reading test
   test_rohet <- read.csv2("detected.ROHet.csv", header = T)
+
+  # sort and add row names
+  # test_rohet <- test_rohet[order(test_rohet$breed, test_rohet$id), ]
+  # row.names(test_rohet) <- 1:nrow(test_rohet)
 
   # compare rohet table
   expect_identical(test_rohet, reference_rohet)
@@ -26,3 +38,13 @@ test_that("n_individuals is equal to 3", {
   file.remove("genotype.raw")
   file.remove("plink.map")
 })
+
+test_that("No file nor dataframe throws error", {
+  # loading dataset
+  data("chillingam")
+
+  # test for errors
+  expect_error(RUNS.run("fake_genotype", chillingham_map), "doesn't exists")
+  expect_error(RUNS.run(chillingham_genotype, "fake_map"), "doesn't exists")
+})
+
