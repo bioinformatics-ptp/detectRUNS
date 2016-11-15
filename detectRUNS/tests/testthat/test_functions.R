65e614338f85d8499be3a25c29b084ae0ed6a14b
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-11-15 11:39:49 +0100

    Add test and examples for a few functions
    test and examples for heteroZygotTest and homoZygotTest were added.
    test fail for slidingWindow (Cpp function not yet implemented)

--- detectRUNS/tests/testthat/test_functions.R
+++ detectRUNS/tests/testthat/test_functions.R
@@ -26,7 +26,7 @@ test_that("Testing snpInRun", {
   # get animals
   animals <- genotype[ ,c(1,2)]
 
-  #remove unnecessary fields from the .raw file
+  # remove unnecessary fields from the .raw file
   genotype <- genotype[ ,-c(1:6)]
 
   # require "plyr"
@@ -69,3 +69,118 @@ test_that("Testing genoConvert", {
   test <- genoConvertCpp(geno012)
   expect_equal(test, geno01)
 })
+
+test_that("Testing slidingWindow", {
+  # importing data
+  data("chillingam")
+
+  # parameters
+  windowSize <- 10
+  threshold <- 0.1
+  minSNP <- 5
+  ROHet <- TRUE
+  maxOppositeGenotype <- 1
+  maxMiss <- 1
+  maxGap <- 10^6
+  minLengthBps <- 1000
+  minDensity <- 1/10
+
+  # get genotype data
+  genotype <- chillingham_genotype
+
+  # get map data
+  mapFile <- chillingham_map
+
+  # get animals
+  animals <- genotype[ ,c(1,2)]
+
+  # remove unnecessary fields from the .raw file
+  genotype <- genotype[ ,-c(1:6)]
+
+  # define an internal function
+  is_run <- function(x, animal) {
+    gaps <- diff(mapFile$bps)
+
+    # call R function
+    y <- slidingWindow(x, gaps, windowSize, step=1, ROHet=ROHet, maxOppositeGenotype, maxMiss, maxGap);
+
+    # call cppFunction
+    test <- slidingWindowCpp(x, gaps, windowSize, step=1, ROHet=ROHet, maxOppositeGenotype, maxMiss, maxGap);
+
+    # testing function
+    expect_equal(test, y)
+  }
+
+  for (i in 1:nrow(genotype)) {
+    is_run(
+      as.vector(genotype[i, ]),
+      animal = animals[i, ]
+    )
+  }
+
+})
+
+test_that("Testing homoZygotTest", {
+  # setting parameters
+  maxHom <- 1
+  maxMiss <- 1
+  maxGap <- 10^6
+
+  x <- c(0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  gaps <- c(3721, 3871, 7059, 4486, 7545, 4796, 3043, 9736, 3495, 5051,
+            9607, 6555, 11934, 6410, 3415, 1302, 3110, 6609, 3292)
+  test <- homoZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_true(test)
+
+  # insert two missing values (> naxMIss)
+  x[1:2] <- c(NA, NA)
+  test <- homoZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+
+  # revert, and change maxGap
+  x[1:2] <- c(0, 0)
+  gaps[10] <- maxGap + gaps[10]
+  test <- homoZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+
+  x <- c(0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+         1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+  gaps <- c(2514, 2408, 2776, 2936, 1657, 494, 1436, 680, 909, 678,
+            615, 1619, 2058, 2446, 1085, 660, 1259, 1042, 2135)
+  test <- homoZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+})
+
+test_that("Testing heteroZygotTest", {
+  # setting parameters
+  maxHom <- 1
+  maxMiss <- 1
+  maxGap <- 10^6
+
+  x <- c(0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+         1, 1, 1, 1, 0, 0, 1, 0, 0, 0)
+  gaps <- c(4374, 8744, 5123, 14229, 5344, 690, 8566, 5853, 2369, 3638,
+            4848, 600, 2333, 976, 2466, 2269, 5411, 6021, 4367)
+  test <- heteroZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+
+  x <- c(0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+         1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+  gaps <- c(2514, 2408, 2776, 2936, 1657, 494, 1436, 680, 909, 678,
+            615, 1619, 2058, 2446, 1085, 660, 1259, 1042, 2135)
+  test <- heteroZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_true(test)
+
+  # insert two missing values (> naxMIss)
+  x[1:2] <- c(NA, NA)
+  test <- heteroZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+
+  # revert, and change maxGap
+  x[1:2] <- c(0, 1)
+  gaps[10] <- maxGap + gaps[10]
+  test <- heteroZygotTest(x, gaps, maxHom, maxMiss, maxGap)
+  expect_false(test)
+})
+
