d3a27a73f076d1f645755e30b83d5d33c9801bd1
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-11-18 17:29:50 +0100

    No files were written
    I've removed all write.table instructions from libraries. Functions
    returns data.frame
    
    
    Former-commit-id: eb1ed43fb5933967df17ca2bad3701b7918d6497

--- detectRUNS/R/run.R
+++ detectRUNS/R/run.R
@@ -19,7 +19,7 @@
 #' @param minLengthBps minimum length of run in bps (defaults to 1000 bps = 1 kbps)
 #' @param minDensity minimum n. of SNP per kbps (defaults to 0.1 = 1 SNP every 10 kbps)
 #'
-#' @return n. of individuals for which runs have been written out
+#' @return a dataframe with RUNs of Homozygosity or Heterozygosity
 #' @export
 #'
 #' @import plyr
@@ -36,10 +36,13 @@
 #' ROHet = TRUE, maxOppositeGenotype = 1, maxMiss = 1,  minLengthBps = 1000, minDensity = 1/10)
 #'
 
+# TODO add output file parameter
 RUNS.run <- function(genotype, mapFile, windowSize = 15, threshold = 0.1, minSNP = 3, ROHet = TRUE,
                      maxOppositeGenotype = 1, maxMiss = 1, maxGap = 10^6, minLengthBps = 1000, minDensity = 1/10) {
 
-    # debug
+  # TODO: check for output_file existance
+
+  # debug
   if (ROHet == TRUE) {
     message("Analysing Runs of Heterozygosity (ROHet)")
   } else if (ROHet == FALSE) {
@@ -56,7 +59,7 @@ RUNS.run <- function(genotype, mapFile, windowSize = 15, threshold = 0.1, minSNP
     if(file.exists(genotype)){
       genotype_path <- genotype
       # using bigmemory to read data
-      genotype <- read.big.matrix(genotype_path, sep = " ", header = T, type = "integer")
+      genotype <- read.big.matrix(genotype_path, sep = " ", header = T, type = "short")
       colClasses <- c(
         rep("character", 2),
         rep("NULL", ncol(genotype)-2)
@@ -82,43 +85,64 @@ RUNS.run <- function(genotype, mapFile, windowSize = 15, threshold = 0.1, minSNP
     }
   }
 
+  # TODO: check that genotype columns and mapFile rows (+6) are identical
+
   # setting colnames
   names(mapFile) <- c("Chrom","SNP","cM","bps")
 
   #remove unnecessary fields from the .raw file
   genotype <- genotype[ ,-c(1:6)]
 
-  ## write out populations/individuals for further plots (snpInRuns)
-  utils::write.table(animals, file="genotype.raw", quote=FALSE, row.names=FALSE, col.names=TRUE)
-
-  report_filename <- paste("detected",ifelse(ROHet,"ROHet","ROHom"),"csv",sep=".")
-  if(file.exists(report_filename)) file.remove(report_filename)
-
-  # require "plyr"
-  n_of_individuals <- vector(length = nrow(genotype))
+  # record all runs in a dataframe
+  RUNs <- data.frame(breed=character(), id=character(), chrom=character(), nSNP=integer(),
+                     from=integer(), to=integer(), lengthBps=integer())
 
   # calculate gaps
   gaps <- diff(mapFile$bps)
 
-  # define an internal function
-  is_run <- function(x, animal) {
+  # define an internal function to call other functions
+  find_run <- function(x, animal) {
+    # get individual and breed
+    ind <- as.character(animal$IID)
+    breed <- as.character(animal$FID)
+
     # use sliding windows
     y <- slidingWindowCpp(x, gaps, windowSize, step=1, maxGap, ROHet=ROHet, maxOppositeGenotype, maxMiss);
     snpRun <- snpInRunCpp(y,windowSize,threshold)
     dRUN <- createRUNdf(snpRun,mapFile,minSNP,minLengthBps,minDensity)
 
-    # this function will write ROH on report_filename (defined inside writeRUN)
-    is_run <- writeRUN(as.character(animal$IID),dRUN,ROHet,as.character(animal$FID))
-    return(is_run)
+    # manipulate dRUN to order columns
+    dRUN$id <- rep(ind, nrow(dRUN))
+    dRUN$breed <- rep(breed, nrow(dRUN))
+    dRUN <- dRUN[,c(7,6,4,3,1,2,5)]
+
+    # debug
+    if(nrow(dRUN) > 0) {
+      message(paste("N. of RUNS for individual", ind, "is:", nrow(dRUN)))
+    } else {
+      message(paste("No RUNs found for animal",ind))
+    }
+
+    #return RUNs to caller
+    return(dRUN)
   }
 
   for (i in 1:nrow(genotype)) {
-    n_of_individuals[i] <- is_run(
+    a_run <- find_run(
       as.integer(genotype[i, ]),
       animal = animals[i, ]
     )
+
+    # bind this run (if has rows) to others RUNs (if any)
+    RUNs <- rbind(RUNs, a_run)
   }
 
-  return(sum(n_of_individuals))
+  # fix row names
+  row.names(RUNs) <- NULL
+
+  # TODO: write output file if requested
+
+  # return calculated runs (data.frame)
+  return(RUNs)
 }
 
