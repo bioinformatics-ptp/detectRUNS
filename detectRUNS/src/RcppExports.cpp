1f790fc445a463d092b6444c5089b48e0347bc89
Paolo Cozzi <bunop@libero.it>
2022-03-28 19:06:35 +0200

    :white_check_mark: test tableRuns

--- detectRUNS/src/RcppExports.cpp
+++ detectRUNS/src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // fast_factor
 SEXP fast_factor(SEXP x);
 RcppExport SEXP _detectRUNS_fast_factor(SEXP xSEXP) {
